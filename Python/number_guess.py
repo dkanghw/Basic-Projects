26f34028ddb0fb5f5d53a19bcceaa9957bdf80ee
dkanghw <d.kanghw@gmail.com>
2023-10-27 11:59:18 +0800

    guess function

--- Python/number_guess.py
+++ Python/number_guess.py
@@ -1,4 +1,6 @@
 import re
+import math
+import random
 
 
 def input_filter(word):
@@ -11,16 +13,40 @@ def input_filter(word):
     return guess_matches, set_matches
 
 
-def guess_function():
-    print("Guess")
+def guess_Function():
+
+    attempt_count = 0
+    random_value = random_Number()
+
+    while (True):
+        attempt_count += 1
+        number_pick = int(
+            input("Pick a number between 1 and 100 inclusive\n"))
+        if (number_pick < random_value):
+            print("Too low, Try again.\n")
+        elif (number_pick > random_value):
+            print("Too high, Try again.\n")
+        else:
+            print("Correct")
+            break
+
+    print("Attempted count: %i " % attempt_count)
 
 
-def set_function():
+def set_Function():
     print("Set")
 
 
 def unknownWord_ErrorCatch():
     print("Unknown Word")
+
+
+def random_Number():
+    min_value = 1
+    max_value = 100
+    random_value = random.randint(min_value, max_value)
+
+    return random_value
 
 
 def main():
@@ -29,13 +55,10 @@ def main():
     guess_matches, set_matches = input_filter(input_word)
 
     for match in guess_matches:
-        guess_function()
+        guess_Function()
 
     for match in set_matches:
-        set_function()
-
-    if (not guess_matches or not set_matches):
-        unknownWord_ErrorCatch()
+        set_Function()
 
 
 if __name__ == "__main__":
